d0b7e4b38ac7a1d43fc8c7cbd12d7403ddba75d8
Camill Kettouch <camillkettouch@MacBook-Air-de-Camill.local>
2026-04-19 01:41:42 +0400

    Switch back to blog.shortmotion.fr: remove base /blog, update internal links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://shortmotion.fr',
-  base: '/blog',
+  site: 'https://blog.shortmotion.fr',
   integrations: [sitemap(), mdx()],
   markdown: {
     shikiConfig: {
